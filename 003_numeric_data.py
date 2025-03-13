bdc2cfe9d342d3352193620ff3f5196d9ffcb1ef
mutato12345 <awotunjihabeeb@gmail.com>
2025-03-13 17:58:40 +0100

    003_numeric_data.py

---
+++ 003_numeric_data.py
@@ -0,0 +1,40 @@
+
+code_1 = 5
+
+code_2 = 3.4285
+
+print(code_1 + code_2)
+
+print(code_1 - code_2)
+
+print(code_1 * code_2)
+
+print(code_1 / code_2)
+
+print(code_1 // code_2)
+
+print(code_1 ** code_2)
+
+print(code_1 % code_2)
+
+print((10 + 3) * 2 ** 2 / 4 - 1)
+
+print(abs(-15))
+
+print(round(code_2, 1))
+
+print(round(code_2, 2))
+
+print(code_1 == code_2)
+
+print(code_1 != code_2)
+
+print(code_1 > code_2)
+
+print(code_1 < code_2)
+
+print(code_1 >= code_2)
+
+print(code_1 <= code_2)
+
+print(int(code_2))
