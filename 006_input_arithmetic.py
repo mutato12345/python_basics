2ccc13da64bf86692b82c3afb1be8c89c8ee1e9d
mutato12345 <awotunjihabeeb@gmail.com>
2025-03-21 13:11:09 +0100

    006_input_arithmetic.py

---
+++ 006_input_arithmetic.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number(integer):"))
+num2 = float(input("Enter the second number(float):")) 
+num3 = num1 + num2
+num4 = num1 - num2
+num5 = num1 * num2
+num6 = num1 / num2
+
+print(num3)
+
+print(num4)
+
+print(num5)
+
+print(num6)
---
+++ 007_extract_extention.py
@@ -0,0 +1,4 @@
+filename = ("Enter a file name: ")
+filename_list = filename.split('.')
+
+print(filename_list[-1])
