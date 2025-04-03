fae34fd6d85bdbfb95074792d08aeb63138083bc
mutato12345 <awotunjihabeeb@gmail.com>
2025-04-03 16:34:21 +0100

    026_function.py

---
+++ 026_function.py
@@ -0,0 +1,15 @@
+def greet_user():
+    print("Hello, welcome to python programming!")
+greet_user()
+
+def add_numbers(a, b):
+    print(a + b)
+add_numbers(10, 5)
+
+def multiply_numbers(a, b):
+    return a * b
+
+result = multiply_numbers(4, 3)
+print(result)
+
+
