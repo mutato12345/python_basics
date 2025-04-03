7461c078e508094f591fa03e0ae1a75debf85122
mutato12345 <awotunjihabeeb@gmail.com>
2025-04-03 13:59:34 +0100

    022_for_loop.py

--- 020_while_loop.py
+++ 020_while_loop.py
@@ -2,4 +2,4 @@ start = int(input("Enter the starting number: "))
 end = int(input("Enter the ending number: "))
 while start <= end:
     print(start)
-    start += 1
+    start += :1
---
+++ 022_for_loop.py
@@ -0,0 +1,9 @@
+start_num = int(input("Enter the starting number: "))
+end_num = int(input("Enter the ending number: "))
+
+for num in range (start_num, end_num):
+
+    if num % 2 == 0:
+       print(num)
+
+
