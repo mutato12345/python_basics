ca82e2553b1e8f393cecc83d9b6632bcfcec536e
mutato12345 <awotunjihabeeb@gmail.com>
2025-04-05 12:59:14 +0100

    028_main.py

---
+++ 028_main.py
@@ -0,0 +1,16 @@
+from math_027_operation import add, subtract, multiply, divide
+
+a = 10
+b = 5
+
+c = add(a, b)
+print(c)
+
+c = subtract(a, b)
+print(c)
+
+c = multiply(a, b)
+print(c)
+
+c = divide(a, b)
+print(c)
