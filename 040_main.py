ee697c9975667db1dd8958dde03670981f8c6164
mutato12345 <awotunjihabeeb@gmail.com>
2025-04-13 16:55:29 +0100

    040_main.py

---
+++ 040_main.py
@@ -0,0 +1,20 @@
+import logic_039
+
+input_string = "hello world"
+upper_result = logic_039.convert_str_to_upper(input_string)
+print("Uppercase version:", upper_result)
+
+today_date = logic_039.get_today_date()
+print("Today's date:", today_date)
+
+vowel_count = logic_039.count_vowels(test_string)
+print("Number of vowels:", vowel_count)
+
+ 
+random_number = logic_039.get_random_number()
+print("Random number:", random_number)
+
+full_name = logic_039.get_full_name()
+print("Full name:", full_name)
+
+
---
+++ logic_039.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import random
+
+def convert_str_to_upper(input_str):
+    return input_str.upper()
+
+def get_today_date():
+    return datetime.today().strftime("%y-%m-%d)
+
+    
+    def count_vowels(input_str):
+    vowels = "aeiou"
+    return sum(1 for char in input_str.lower() if char in vowels)
+
+    def get_random_numbers():
+    return random.randint(1, 100)
+
+    def get_full_namer():
+    retur "Habeeb Opeyemi"
+
+
+
